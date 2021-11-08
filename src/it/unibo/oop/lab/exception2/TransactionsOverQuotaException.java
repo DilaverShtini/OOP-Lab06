854d104f28947d83d9f108b7e7f2f2e463185f95
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2021-11-08 17:40:49 +0100

    Rename TransactionsOverQuotaExceptions (without the last s)

--- src/it/unibo/oop/lab/exception2/TransactionsOverQuotaException.java
+++ src/it/unibo/oop/lab/exception2/TransactionsOverQuotaException.java
@@ -1,6 +1,6 @@
 package it.unibo.oop.lab.exception2;
 
-public class TransactionsOverQuotaExceptions extends IllegalStateException{
+public class TransactionsOverQuotaException extends IllegalStateException{
 	
 	private static final long serialVersionUID = 1L;
 
