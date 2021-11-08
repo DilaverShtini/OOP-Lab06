0e7f92a9b754321c3e2b47fc5e28e635f6303e10
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2021-11-08 16:18:10 +0100

    Implemented TransactionsOverQuotaExceptions

---
+++ src/it/unibo/oop/lab/exception2/TransactionsOverQuotaExceptions.java
@@ -0,0 +1,15 @@
+package it.unibo.oop.lab.exception2;
+
+public class TransactionsOverQuotaExceptions extends IllegalStateException{
+	
+	private static final long serialVersionUID = 1L;
+
+	public String toString() {
+		return "Too many transactions...";
+	}
+	
+	public String getMessage() {
+        return this.toString();
+    }
+	
+}
